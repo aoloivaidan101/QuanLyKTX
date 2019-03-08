58b279ba5d9319bc4e806c2146643eecc9821338
ThanHoang <ThanHoang@DESKTOP-K0913AQ>
2019-03-08 23:35:51 +0700

    Revert "update new"
    This reverts commit ab844d3de108f092e1ff9b60ec297d111273e24d.

--- QuanLyKTX/TestThoi.cs
+++ QuanLyKTX/TestThoi.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace QuanLyKTX
 {
-    public partial class Home : UserControl
+    public partial class TestThoi : UserControl
     {
-        public Home()
+        public TestThoi()
         {
             InitializeComponent();
         }
